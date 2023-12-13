79827bc5419198e18c9442b4c4a5bd5a440efe9d
belmoussaoui <emb-12@outlook.com>
2023-12-13 16:04:32 +0100

    Add Paddle component and up/down move

--- frontend/src/pages/play/Play.jsx
+++ frontend/src/pages/play/Play.jsx
@@ -1,27 +1,59 @@
 import { useEffect, useState, useRef } from "react";
 import { Canvas, useFrame } from '@react-three/fiber'
 import "./Play.css"
+import { forwardRef } from 'react';
 
-function Box(props) {
-    const meshRef = useRef()
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    useFrame((state, delta) => (
-        console.log("ok")
-    ))
+function useKeyboard() {
+    const keyMap = useRef({})
+  
+    useEffect(() => {
+      const onDocumentKey = (e) => {
+        keyMap.current[e.code] = e.type === 'keydown'
+      }
+      document.addEventListener('keydown', onDocumentKey)
+      document.addEventListener('keyup', onDocumentKey)
+      return () => {
+        document.removeEventListener('keydown', onDocumentKey)
+        document.removeEventListener('keyup', onDocumentKey)
+      }
+    })
+  
+    return keyMap.current
+  }
+
+const Paddle = forwardRef(function Paddle(props, ref) {
     return (
         <mesh
+            ref={ref}
             {...props}
-            ref={meshRef}
-            scale={active ? 1 : 1}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
+        >
             <boxGeometry args={[0.5, 2, 0.5]} />
             <meshStandardMaterial color={'black'} />
         </mesh>
     )
   }
+  )
+
+function Logic() {
+    const meshRef1 = useRef()
+    const meshRef2 = useRef()
+
+    useFrame((state, delta) => {
+        console.log(meshRef1);
+        keyMap['KeyW'] && (meshRef1.current.position.y += 3 * delta)
+        keyMap['KeyS'] && (meshRef1.current.position.y -= 3 * delta)
+        keyMap['ArrowUp'] && (meshRef2.current.position.y += 3 * delta)
+        keyMap['ArrowDown'] && (meshRef2.current.position.y -= 3 * delta)
+    })
+    const keyMap = useKeyboard()
+
+    return <>
+        <ambientLight intensity={10} />
+        <pointLight position={[0, 1, -2]} />
+        <Paddle ref={meshRef1} position={[-5, 0, 0]} />
+        <Paddle ref={meshRef2} position={[5, 0, 0]} />
+    </>
+}
 
 function Play() {
     const gameSocket = new WebSocket('ws://localhost:8080/ws/game/');
@@ -35,16 +67,12 @@ function Play() {
             const data = JSON.parse(e.data);
             console.log(data);
         };
-    }, []);
-    
+    }, []);    
     return (
         <div className="container-fluid">
             <div className="row">
             <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 100]}}>
-                <ambientLight intensity={10} />
-                <pointLight position={[0, 1, -2]} />
-                <Box position={[-5, 0, 0]} />
-                <Box position={[5, 0, 0]} />
+                <Logic></Logic>
             </Canvas>
              </div>
         </div>

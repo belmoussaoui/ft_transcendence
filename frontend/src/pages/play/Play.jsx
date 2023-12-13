943347c7160c1ddc0ed17aaf8ae09004fe2b7f5e
belmoussaoui <emb-12@outlook.com>
2023-12-13 15:29:44 +0100

    Add React Three Fiber

--- frontend/src/pages/play/Play.jsx
+++ frontend/src/pages/play/Play.jsx
@@ -1,5 +1,27 @@
-import { useEffect, useState } from "react";
-import Block from "../../components/block/Block"
+import { useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from '@react-three/fiber'
+import "./Play.css"
+
+function Box(props) {
+    const meshRef = useRef()
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    useFrame((state, delta) => (
+        console.log("ok")
+    ))
+    return (
+        <mesh
+            {...props}
+            ref={meshRef}
+            scale={active ? 1 : 1}
+            onClick={(event) => setActive(!active)}
+            onPointerOver={(event) => setHover(true)}
+            onPointerOut={(event) => setHover(false)}>
+            <boxGeometry args={[0.5, 2, 0.5]} />
+            <meshStandardMaterial color={'black'} />
+        </mesh>
+    )
+  }
 
 function Play() {
     const gameSocket = new WebSocket('ws://localhost:8080/ws/game/');
@@ -18,9 +40,13 @@ function Play() {
     return (
         <div className="container-fluid">
             <div className="row">
-                <Block title="Play Match">
-                </Block>
-            </div>
+            <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 100]}}>
+                <ambientLight intensity={10} />
+                <pointLight position={[0, 1, -2]} />
+                <Box position={[-5, 0, 0]} />
+                <Box position={[5, 0, 0]} />
+            </Canvas>
+             </div>
         </div>
     )
 }

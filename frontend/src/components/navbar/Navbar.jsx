77781dac43a8f7af547bd36ec69e899a2d3c36b2
alondot <alondot@gmail.com>
2023-12-19 15:06:15 +0100

    Update navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
-import "./Navbar.css"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Assurez-vous d'avoir importé Bootstrap dans votre application React
 
 function Navbar() {
-    return (
-        <nav className="navbar-main"></nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Ft_transcendence</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="/">Home</a>
+            <a className="nav-link" href="/play/">Play</a>
+            <a className="nav-link" href="/tournament/">Tournament</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
